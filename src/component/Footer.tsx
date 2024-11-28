27b0253dc8d3416862a6fc5bca044c7acf4aafae
Faiz2979 <faizannabil877@gmail.com>
2024-11-28 22:30:27 +0700

    Update Footer.tsx

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -2,34 +2,33 @@ import { FaGithub, FaInstagram, FaItchIo, FaLinkedin } from "react-icons/fa6";
 
 export default function Footer() {
     return (
-        <div className="flex items-center justify-between w-full h-24 p-20">
-            <div className="oxanium oxanium-semibold">
-                <h1 className="text-4xl">Contact me at faizannabil877@gmail.com</h1>
-                <p>© 2021 Muhammad Faiz Annabil</p>
+        <div className="flex flex-col md:flex-row items-center justify-between w-full h-auto p-10">
+            <div className="oxanium oxanium-semibold text-center md:text-left mb-6 md:mb-0">
+                <h1 className="text-sm md:text-md lg:text-4xl">Contact me at faizannabil877@gmail.com</h1>
+                <p className="text-xs md:text-md">© 2021 Muhammad Faiz Annabil</p>
             </div>
-            <div id="iconButton" className="flex flex-row gap-4">
+            <div id="iconButton" className="flex flex-row md:flex-col lg:flex-row gap-2 md:gap-6">
                 <a
                     className="flex items-center justify-center hover:text-blue-500"
                     href="https://github.com/faiz2979">
-                    <FaGithub className="w-8 h-8" />
+                    <FaGithub className="w-6 h-6 md:w-8 md:h-8" />
                 </a>
                 <a
                     className="flex items-center justify-center hover:text-blue-500"
                     href="https://www.linkedin.com/in/faizannabil/">
-                    <FaLinkedin className="w-8 h-8" />
+                    <FaLinkedin className="w-6 h-6 md:w-8 md:h-8" />
                 </a>
                 <a
                     className="flex items-center justify-center hover:text-blue-500"
                     href="https://flamexq.itch.io/">
-                    <FaItchIo className="w-8 h-8" />
+                    <FaItchIo className="w-6 h-6 md:w-8 md:h-8" />
                 </a>
                 <a
                     className="flex items-center justify-center hover:text-blue-500"
                     href="https://flamexq.itch.io/">
-                    <FaInstagram className="w-8 h-8" />
+                    <FaInstagram className="w-6 h-6 md:w-8 md:h-8" />
                 </a>
             </div>
-
         </div>
     );
 }
